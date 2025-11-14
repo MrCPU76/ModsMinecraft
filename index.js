61c028ef3c9e2e0f735c37527beadfd21d10bb4a
oliverfernandez <oliverfernandez@alumnat.iesdamiahuguet.net>
2025-11-14 10:41:05 +0100

    Pagina con botones

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+let nombreProductes = 0
+let gigas =0
+
+function Comprar(nombre, peso){
+      document.getElementById("cistella").innerText += "GB añadido\n"
+      nombreProductes +=1 // nombreProductes = nombreProductes + 1;
+     document.getElementById('comptadorProductes').innerText = nombreProductes + " articles"
+}
